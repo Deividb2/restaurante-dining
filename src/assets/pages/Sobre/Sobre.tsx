25e9dd5329542a79e8832876cebe7f21f6c811f6
Deivid Barreto <deividbarreto57@gmail.com>
2024-02-05 13:51:57 -0300

    add componentes da page 'Sobre'

--- src/assets/pages/Sobre/Sobre.tsx
+++ src/assets/pages/Sobre/Sobre.tsx
@@ -1,4 +1,9 @@
 import HeaderPages from "../../components/HeaderPages"
+import Purpose from "./components/Purpose"
+import Outset from "./components/Outset"
+import Feedbacks from "./components/Feedbacks"
+
+// img e icon
 import imgSobre from '../../../../public/imgs/BM1.jpg'
 import { IoMdPeople } from "react-icons/io"
 
@@ -8,6 +13,9 @@ export default function Sobre() {
             <HeaderPages title="Sobre nós" subtitle="Como sabe como chegamos até aqui?" fundo={imgSobre} >
                 <IoMdPeople className="text-4xl md:text-6xl" />
             </HeaderPages>
+            <Purpose />
+            <Outset />
+            <Feedbacks />
         </>
     )
 }
